da9b298a2aba1c79693e6f33cd5e66a3524718e0
orlandoterceiro <orlando.terceiro@outlook.com>
2023-10-06 20:00:21 -0300

    desafio pronto

---
+++ desafio1.js
@@ -0,0 +1,12 @@
+let nome = "Joaquim Cata-Cana"
+let XP = 9500
+
+if (XP <= 1000){console.log("O herói " + nome + " está no nível Ferro")}
+else if (XP <= 2000){console.log("O herói " + nome + " está no nível Bronze")}
+else if (XP <= 5000){console.log("O herói " + nome + " está no nível Prata")}
+else if (XP <= 7000){console.log("O herói " + nome + " está no nível Ouro")}
+else if (XP <= 8000){console.log("O herói " + nome + " está no nível Platina")}
+else if (XP <= 9000){console.log("O herói " + nome + " está no nível Ascendente")}
+else if (XP <= 10000){console.log("O herói " + nome + " está no nível Imortal")}
+else if (XP >= 10001){console.log("O herói " + nome + " está no nível Radiante")}
+
